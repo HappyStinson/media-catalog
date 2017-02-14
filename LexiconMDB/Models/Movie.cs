e3053c2463000069441caaaade6face2b10f40e2
Rasmus Nordling <rasmus.nordling@gmail.com>
2017-02-14 21:11:44 +0100

    Initial commit

---
+++ LexiconMDB/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LexiconMDB.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+        public int? Length { get; set; }
+        public string Genre { get; set; }
+        public int? AgeLimit { get; set; }
+        public int? MetaScore { get; set; }
+
+    }
+}
